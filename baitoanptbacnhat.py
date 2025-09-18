798102e5426e2a9a831cc3515a76a1d42cb9afbd
baochan50 <duonghoangbao25@gmail.com>
2025-09-18 07:48:08 +0700

    Add files via upload

---
+++ baitoanptbacnhat.py
@@ -0,0 +1,8 @@
+print ("Giả sử phương trình bậc nhất có dạng: ax+b=0")
+a= float(input("Nhập vào số a:"))
+b= float(input("Nhập vào số b:"))
+if a==0 :
+   print("Phương trình vô nghiệm")
+else:
+    x=-b/a
+    print("Đáp số là: ", x)
